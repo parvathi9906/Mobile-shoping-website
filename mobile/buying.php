0c8d6871521412dc5c1fd479ee998cc616a90c36
PARVATHI N K <72162607+parvathi9906@users.noreply.github.com>
2020-12-02 11:52:54 +0530

    Add files via upload

---
+++ mobile/buying.php
@@ -0,0 +1,12 @@
+<?php
+ob_start();
+// include header.php file
+include ('header2.php');
+?>
+<?php
+
+/*  include top sale section */
+        include ('Template/_buying.php');
+/*  include top sale section */
+
+?>
